2b7c81a47eb003d4eba26cd925c8f07bd0e5c495
verobelo <142001535+verobelo@users.noreply.github.com>
2025-04-05 11:24:48 +0200

    Add components folder and Header & Footer components

---
+++ cv-creator/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer>
+      <a
+        href="https://www.flaticon.com/free-icons/typewriter"
+        title="typewriter icons">
+        Typewriter icons created by <strong>ghost_icon</strong> - Flaticon
+      </a>
+    </footer>
+  );
+}
---
+++ cv-creator/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="./public/typewriter.png" alt="logo" width={48} height={48} />
+      <h2>My Resume Writer.</h2>
+    </header>
+  );
+}
