b89a001b7876d139145caac6387e0c0911933f45
verobelo <142001535+verobelo@users.noreply.github.com>
2025-04-05 11:25:12 +0200

    Add imports

--- cv-creator/src/App.jsx
+++ cv-creator/src/App.jsx
@@ -1,3 +1,6 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="cv-container">
@@ -7,26 +10,6 @@ function App() {
   );
 }
 
-function Header() {
-  return (
-    <header>
-      <img src="./public/typewriter.png" alt="logo" width={48} height={48} />
-      <h2>My Resume Writer.</h2>
-    </header>
-  );
-}
 function CVForm() {}
 function CVPreview() {}
-function Footer() {
-  return (
-    <footer>
-      <a
-        href="https://www.flaticon.com/free-icons/typewriter"
-        title="typewriter icons">
-        Typewriter icons created by <strong>ghost_icon</strong> - Flaticon
-      </a>
-    </footer>
-  );
-}
-
 export default App;
