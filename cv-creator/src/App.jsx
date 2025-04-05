7a06f687e8a7204cd53b798e67146fbb4e4806a6
verobelo <142001535+verobelo@users.noreply.github.com>
2025-04-05 20:55:44 +0200

    Add Personal Info component

--- cv-creator/src/App.jsx
+++ cv-creator/src/App.jsx
@@ -5,23 +5,78 @@ function App() {
   return (
     <div className="cv-container">
       <Header />
+      <CVForm />
+      <CVPreview />
       <Footer />
     </div>
   );
 }
 
 function CVForm() {
-  <div className="cv-form">
-    <PersonalInfoForm />
-    <Summary />
-  </div>;
- 
-
+  return (
+    <div className="cv-form">
+      <PersonalInfoForm />
+      {/*<Summary />
+    <SkillsList />
+    <ExperienceList />
+    <ProjectsList />
+    <Education />*/}
+    </div>
+  );
 }
-function CVPreview() {}
 
+function PersonalInfoForm() {
+  return (
+    <section className="personal-info">
+      <div className="personal-info header">
+        <img src="./public/down-arrow.png" width={25} height={25} />
+        <h1>Personal Information</h1>
+      </div>
+      <div className="personal-info__form">
+        <label>
+          Full Name:
+          <input type="text" name="name" value="" />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" value="" />
+        </label>
+        <label>
+          Phone Number:
+          <input type="tel" name="phone" value="" />
+        </label>
+        <label>
+          Address:
+          <input type="text" name="address" value="" />
+        </label>
+        <label>
+          Linkedin:
+          <input type="text" name="linkedin" value="" />
+        </label>
+        <label>
+          GitHub:
+          <input type="text" name="github" value="" />
+        </label>
+        <label>
+          Personal Website:
+          <input type="text" name="website" value="" />
+        </label>
+      </div>
+    </section>
+  );
+}
 
+function Summary() {
+  <section className="summary">
+    <div className="summary header">
+      <img src="./public/down-arrow.png" width={25} height={25} />
+      <h1>Personal Information</h1>
+    </div>
+  </section>;
+}
 
-
+function CVPreview() {
+  return <div className="cv-preview">CV Preview</div>;
+}
 
 export default App;
