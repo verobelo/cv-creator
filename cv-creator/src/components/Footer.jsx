c2740b0b379843eb95d75108e1e79a47afc6c884
verobelo <142001535+verobelo@users.noreply.github.com>
2025-04-05 20:56:21 +0200

    Modified link in the footer

--- cv-creator/src/components/Footer.jsx
+++ cv-creator/src/components/Footer.jsx
@@ -2,9 +2,9 @@ export default function Footer() {
   return (
     <footer>
       <a
-        href="https://www.flaticon.com/free-icons/typewriter"
-        title="typewriter icons">
-        Typewriter icons created by <strong>ghost_icon</strong> - Flaticon
+        href="https://www.flaticon.com/free-icons/down-arrow"
+        title="down arrow icons">
+        Icons created by <strong>th studio</strong> - Flaticon
       </a>
     </footer>
   );
