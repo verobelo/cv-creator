5b046c2bfebe5381aed227b993a9a9182700f849
verobelo <142001535+verobelo@users.noreply.github.com>
2025-04-06 13:02:58 +0200

    Change page title

--- cv-creator/src/components/Header.jsx
+++ cv-creator/src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header>
       <img src="./public/typewriter.png" alt="logo" width={48} height={48} />
-      <h2>My Resume Writer.</h2>
+      <h2>Git That Job</h2>
     </header>
   );
 }
