78692827ae5eeae4f45c462bfe3c1808b82c7bc6
verobelo <142001535+verobelo@users.noreply.github.com>
2025-04-10 16:19:38 +0200

    Move PersonalInfo component into components folder

---
+++ cv-creator/src/components/PersonalInfoForm.jsx
@@ -0,0 +1,78 @@
+export default function PersonalInfoForm({ data, updateData }) {
+  return (
+    <section className="personal-info">
+      <div className="personal-info__form form">
+        <label>
+          <strong>Full Name:</strong>
+          <input
+            type="text"
+            name="name"
+            placeholder="e.g. Java SCRIPSTON"
+            value={data.name}
+            onChange={(e) => updateData({ ...data, name: e.target.value })}
+          />
+        </label>
+        <label>
+          <strong>Email:</strong>
+          <input
+            type="email"
+            name="email"
+            placeholder="e.g java.scripston@gmail.com"
+            value={data.email}
+            onChange={(e) => updateData({ ...data, email: e.target.value })}
+          />
+        </label>
+        <label>
+          <strong>Phone Number:</strong>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="e.g. (+123)404-NOT-FOUND"
+            value={data.phone}
+            onChange={(e) => updateData({ ...data, phone: e.target.value })}
+          />
+        </label>
+        <label>
+          <strong>Address:</strong>
+          <input
+            type="text"
+            name="address"
+            placeholder="e.g. Div Street, New Html"
+            value={data.address}
+            onChange={(e) => updateData({ ...data, address: e.target.value })}
+          />
+        </label>
+        <label>
+          <strong>Linkedin:</strong>
+          <input
+            type="text"
+            name="linkedin"
+            placeholder="e.g. linkedin.com/in/javascripston"
+            value={data.linkedin}
+            onChange={(e) => updateData({ ...data, linkedin: e.target.value })}
+          />
+        </label>
+        <label>
+          <strong>GitHub:</strong>
+          <input
+            type="text"
+            name="github"
+            placeholder="e.g. github.io/javascripston"
+            value={data.github}
+            onChange={(e) => updateData({ ...data, github: e.target.value })}
+          />
+        </label>
+        <label>
+          <strong>Personal Website:</strong>
+          <input
+            type="text"
+            name="website"
+            placeholder="e.g. wowimadeawebsite.com"
+            value={data.website}
+            onChange={(e) => updateData({ ...data, website: e.target.value })}
+          />
+        </label>
+      </div>
+    </section>
+  );
+}
