f3bbb55f462ad5fb020beda4b453ad1d109aa7a1
verobelo <142001535+verobelo@users.noreply.github.com>
2025-04-12 10:46:07 +0200

    Import skills components

--- cv-creator/src/App.jsx
+++ cv-creator/src/App.jsx
@@ -6,7 +6,7 @@ import PersonalInfoForm from "./components/PersonalInfoForm";
 import PersonalInfoPreview from "./components/PersonalInfoPreview";
 import Summary from "./components/Summary";
 import SummaryPreview from "./components/SummaryPreview";
-import Skill from "./components/Skill";
+import SkillsList from "./components/SkillsList";
 
 function App() {
   const [personalInfo, setPersonalInfo] = useState({
@@ -113,113 +113,6 @@ function CVForm({
         <EducationList />
       </FoldableSection>*/}
     </div>
-  );
-}
-
-function SkillsList({ data, updateData, onAddGroup, onRemoveGroup }) {
-  return (
-    <section className="skills">
-      <button type="button" onClick={onAddGroup}>
-        + Add Skill Group
-      </button>
-      <ul className="skills__form form">
-        {data.map((group) => (
-          <li key={group.id} className="skill__group">
-            <SkillsGroup
-              id={group.id}
-              name={group.name}
-              skills={group.skills}
-              handleRemove={onRemoveGroup}
-              updateData={updateData}
-            />
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
-}
-
-function SkillsGroup({ id, name, skills, updateData, handleRemove }) {
-  function handleOnChangeGroupName(e) {
-    const newName = e.target.value;
-    updateData((prevGroups) =>
-      prevGroups.map((group) =>
-        group.id === id ? { ...group, name: newName } : group
-      )
-    );
-  }
-
-  function handleOnChangeSkillName(skillId, newName) {
-    updateData((prevGroups) =>
-      prevGroups.map((group) =>
-        group.id === id
-          ? {
-              ...group,
-              skills: group.skills.map((skill) =>
-                skill.id === skillId ? { ...skill, name: newName } : skill
-              ),
-            }
-          : group
-      )
-    );
-  }
-
-  function handleAddSkill() {
-    const newSkill = { name: "", id: crypto.randomUUID() };
-    updateData((prevGroups) =>
-      prevGroups.map((group) =>
-        group.id === id
-          ? { ...group, skills: [...group.skills, newSkill] }
-          : group
-      )
-    );
-  }
-
-  function handleRemoveSkill(skillId) {
-    updateData((prevGroups) =>
-      prevGroups.map((group) =>
-        group.id === id
-          ? {
-              ...group,
-              skills: group.skills.filter((skill) => skill.id !== skillId),
-            }
-          : group
-      )
-    );
-  }
-  return (
-    <>
-      <label>
-        <strong>Skill Group: </strong>
-        <input
-          type="text"
-          name="skill-group"
-          value={name}
-          onChange={handleOnChangeGroupName}
-          placeholder="Frameworks, languages, soft skills etc"
-        />
-      </label>
-      <button aria-label="remove skill group" onClick={() => handleRemove(id)}>
-        x
-      </button>
-
-      <ul className="skills__list">
-        {skills.map((skill) => (
-          <li key={skill.id}>
-            <Skill
-              id={skill.id}
-              name={skill.name}
-              onChange={handleOnChangeSkillName}
-              onRemove={handleRemoveSkill}
-            />
-          </li>
-        ))}
-      </ul>
-
-      <button type="button" onClick={handleAddSkill}>
-        + Add Skill
-      </button>
-    </>
   );
 }
 
