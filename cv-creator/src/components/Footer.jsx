3cc27f9521a01088265afdccd3bfdc2f9830d075
verobelo <142001535+verobelo@users.noreply.github.com>
2025-04-13 19:04:36 +0200

    Add a target and rel on the link

--- cv-creator/src/components/Footer.jsx
+++ cv-creator/src/components/Footer.jsx
@@ -1,7 +1,10 @@
 export default function Footer() {
   return (
     <footer>
-      <a href="https://www.flaticon.com/authors/th-studio">
+      <a
+        href="https://www.flaticon.com/authors/th-studio"
+        target="_blank"
+        rel="noopener noreferrer">
         Icons created by <strong>th studio</strong> - Flaticon
       </a>
     </footer>
