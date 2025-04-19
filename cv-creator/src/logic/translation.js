eec97c37f929cfd84cbbef36369691f279d81f82
verobelo <142001535+verobelo@users.noreply.github.com>
2025-04-19 19:11:55 +0200

    Add translations object

---
+++ cv-creator/src/logic/translation.js
@@ -0,0 +1,18 @@
+export const translations = {
+  en: {
+    personalInfo: "Personal Information",
+    summary: "Summary",
+    skills: "Skills",
+    experience: "Experience",
+    projects: "Projects",
+    education: "Education",
+  },
+  es: {
+    personalInfo: "Información de contacto",
+    summary: "Objectivo profesional",
+    skills: "Habilidades",
+    experience: "Experiencia",
+    projects: "Proyectos",
+    education: "Educación",
+  },
+};
