457cb918290cd0a272570936d740b46a04eb5344
verobelo <142001535+verobelo@users.noreply.github.com>
2025-04-20 13:07:23 +0200

    Add keys for projects section

--- cv-creator/src/logic/translation.js
+++ cv-creator/src/logic/translation.js
@@ -1,5 +1,6 @@
 export const translations = {
   en: {
+    // Personal Info
     personalInfo: "Personal Information",
     summary: "Summary",
     skills: "Skills",
@@ -11,11 +12,14 @@ export const translations = {
     phone: "Phone Number",
     linkedin: "Linkedin",
     github: "Github or Portfolio",
+    // Summary
     summaryPlaceholder:
       "Write a short summary presenting yourself, your best skills, your goals etc",
+    // Skills
     addSkillGroup: "Add Skill Group",
     skillGroup: "Skill Group",
     addSkill: "Add Skill",
+    // Experience
     add: "Add",
     position: "Position",
     company: "Company Name",
@@ -23,8 +27,14 @@ export const translations = {
     startDate: "Start Date",
     endDate: "End Date",
     description: "Description",
+    // Projects
+    projectName: "Project Name",
+    technologies: "Technologies",
+    code: "Code",
+    demo: "Demo",
   },
   es: {
+    // Personal Info
     personalInfo: "Información de contacto",
     summary: "Objectivo profesional",
     skills: "Habilidades",
@@ -36,17 +46,25 @@ export const translations = {
     phone: "Teléfono",
     linkedin: "Linkedin",
     github: "Github o Portafolio",
+    // Summary
     summaryPlaceholder:
       "Escriba un pequeño resumen de su trayectoria profesional, sus habilidades, sus metas etc",
+    // Skills
     addSkillGroup: "Añadir Grupo",
     skillGroup: "Grupo de Habilidades",
     addSkill: "Añadir Habilidad",
+    // Experience
     add: "Añadir",
     position: "Puesto",
     company: "Empresa",
     address: "Ubicación",
     startDate: "Fecha de inicio",
-    endDate: "Fecha de finalización",
+    endDate: "Fecha de fin",
     description: "Funciones y logros",
+    // Projects
+    projectName: "Nombre del proyecto",
+    technologies: "Tecnologías",
+    code: "Código",
+    demo: "Demo",
   },
 };
